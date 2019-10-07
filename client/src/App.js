cec7dc7c57847dabe915f04abff260fcc22441a7
Rodney-Race <rodneyrace@gmail.com>
2019-10-07 17:14:36 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 import SavedList from './Movies/SavedList';
+import { Route } from 'react-router-dom';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,7 +14,12 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route exact path="/" component={MovieList}/>
+        <Route path = '/movies/:id' component = {Movie}/>
+        {/* <Route exact path="/movies/:id" render = {props => <Movie {...props} />}/> */}
+        {/* <Route exact path="/movies/:id" render = {props => <Movie {...props}  movie={movie}/>}/> */}
+      </div>
     </div>
   );
 };
